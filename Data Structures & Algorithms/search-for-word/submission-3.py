4884e98d62b444ed2405ddbd092b25e103e76fe1
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-01 12:59:32 -0700

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,31 @@
+DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if len(word) > len(board) * len(board[0]):
+            return False
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if self.find(board, r, c, word, set([(r, c)])):
+                    return True
+        return False
+    
+
+    def find(self, board, r, c, word, visited):
+        if len(word) == 0 or len(word) == 1 and word[0] == board[r][c]:
+            return True
+
+        if word[0] != board[r][c]:
+            return False
+        
+        for (dr, dc) in DIRECTIONS:
+            new_r, new_c = r + dr, c + dc
+            if not 0 <= new_r < len(board) or not 0 <= new_c < len(board[0]):
+                continue
+            if (new_r, new_c) in visited:
+                continue
+            if self.find(board, new_r, new_c, word[1:], visited | set([(new_r, new_c)])):
+                return True
+        return False
+        
+
