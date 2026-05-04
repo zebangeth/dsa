536a7df698046a080c9e1de4f0a9b51f0a507b4b
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-04 11:37:40 -0700

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,25 @@
+DIGITS = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+}
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        combinations = [""] # 这里的 [""] 不是一个真正的答案，而是一个 seed
+        for digit in digits:
+            letters = DIGITS[digit]
+            new_combinations = []
+            for combination in combinations:
+                for letter in letters:
+                    new_combinations.append(combination + letter)
+            combinations = new_combinations
+        
+        return combinations
