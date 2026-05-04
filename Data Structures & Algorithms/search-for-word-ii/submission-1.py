eb10a1226a354ab64810f1ecd8649371dcbe5f4a
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-04 16:33:24 -0700

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,68 @@
+# 1st step: Implement trie
+class TrieNode: 
+    def __init__(self): 
+        self.children = dict()
+        self.is_word = False
+        self.word = None
+
+class WordDict: 
+    def __init__(self): 
+        self.root = TrieNode()
+    
+    def add_word(self, word): 
+        cur = self.root
+        for c in word: 
+            if c not in cur.children: 
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.is_word = True
+        cur.word = word
+    
+    def search(self, word): 
+        cur = self.root
+        for c in word: 
+            if c not in cur.children: 
+                return False
+            cur = cur.children[c]
+        return cur.is_word
+            
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # 2nd step: Store all words into a trie
+        word_dict = WordDict()
+        for word in words: 
+            word_dict.add_word(word)
+        
+        self.output = []
+        root = word_dict.root
+        memo = dict()
+        # 3rd step: Search each cell using a dfs approach
+        for r in range(len(board)): 
+            for c in range(len(board[0])):
+                if board[r][c] not in root.children:
+                    continue
+                self._dfs(r, c, root, board, set())    
+        
+        return self.output
+    
+    def _dfs(self, r, c, node, board, visited):
+        if not self._is_valid(r, c, node, board, visited): 
+            return
+        
+        char = board[r][c]
+        node = node.children[char]
+        if node.is_word:
+            self.output.append(node.word)
+            node.is_word = False  # 避免重复加入同一个 word
+        visited.add((r, c))
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            self._dfs(r + dr, c + dc, node, board, visited)
+        visited.remove((r, c))
+    
+    def _is_valid(self, r, c, node, board, visited):
+        return (
+            0 <= r < len(board)
+            and 0 <= c < len(board[0])
+            and (r, c) not in visited
+            and board[r][c] in node.children
+        )
