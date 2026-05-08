4959893e8185a2f88c92759d5d24307a5f05ba47
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-07 18:04:15 -0700

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = dict()
+        return self.dfs(s, 0, memo)
+
+    def dfs(self, s, start, memo):
+        if start in memo:
+            return memo[start]
+
+        if start == len(s):
+            return 1
+        
+        if s[start] == '0':
+            return 0
+        
+        if start + 1 < len(s) and int(s[start:start + 2]) <= 26:
+            memo[start] = self.dfs(s, start + 1, memo) + self.dfs(s, start + 2, memo)
+            return memo[start]
+        else:
+            memo[start] = self.dfs(s, start + 1, memo)
+            return memo[start]
