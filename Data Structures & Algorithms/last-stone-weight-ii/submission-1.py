78744a77219bb7fcfe92e5d1b4c3ccd25f515555
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-08 15:49:38 -0700

    Add: last-stone-weight-ii - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        total = sum(stones)
+        half = total // 2 + total % 2
+        odd = total % 2
+
+        # state: dp[n][w] stores the max weight can get using stones[:n] <= w
+        dp = [[0] * (half + 1) for _ in range(len(stones) + 1)]
+
+        # initialization
+
+        # state transition function
+        for n in range(1, len(stones) + 1):
+            for w in range(half + 1):
+                if w >= stones[n - 1]:
+                    # option 1: take the current stone
+                    dp[n][w] = max(dp[n][w], stones[n - 1] + dp[n - 1][w - stones[n - 1]])
+                # option 2: not take the current stone
+                dp[n][w] = max(dp[n][w], dp[n - 1][w])
+
+        print(dp)
+        # answer
+        return abs(dp[len(stones)][half] - (total - dp[len(stones)][half]))
