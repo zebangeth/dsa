daa247ff93c2c2f24a92a684127cf8a89b599e9c
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-11 15:26:50 -0700

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        counter = collections.Counter(hand)
+        for num in set(hand):
+            if num not in counter:
+                continue
+            # find the sequence start
+            while num - 1 in counter:
+                num -= 1
+            
+            # check this group
+            for i in range(groupSize):
+                if num + i not in counter:
+                    return False
+                counter[num + i] -= 1
+                if counter[num + i] == 0:
+                    counter.pop(num + i)
+            
+        return True
+            
