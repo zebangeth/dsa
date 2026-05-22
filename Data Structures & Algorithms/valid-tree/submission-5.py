19ff704dfea8c379f63310ffa537982daa36440f
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-22 13:56:40 -0700

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graph = {i: set() for i in range(n)}
+        for (p1, p2) in edges:
+            graph[p1].add(p2)
+            graph[p2].add(p1)
+        
+        visited = set([0])
+        queue = collections.deque([(0, -1)])
+        while queue:
+            cur, parent = queue.popleft()
+            for nei in graph[cur]:
+                if nei in visited and nei != parent:
+                    return False
+                if nei == parent:
+                    continue
+                queue.append((nei, cur))
+                visited.add(nei)
+        return len(visited) == n
