b7a1084c35c33a958f3f082728fabe857b661779
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-26 12:24:44 -0700

    Add: path-with-minimum-effort - submission-3

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        visited = set()
+        heap = [(0, 0, 0)]
+        max_diff = 0
+        while heap:
+            diff, r, c = heapq.heappop(heap)
+            # Dijkstra 中应该是：从 heap pop 出来时，才说明这个点的最优 effort 已经确定，才更新 visited
+            visited.add((r, c))
+            max_diff = max(max_diff, diff)
+            if r == len(heights) - 1 and c == len(heights[0]) - 1:
+                return max_diff
+
+            for (dr, dc) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = r + dr, c + dc
+                if not 0 <= nr < len(heights) or not 0 <= nc < len(heights[0]):
+                    continue
+                if (nr, nc) in visited:
+                    continue
+                ndiff = abs(heights[nr][nc] - heights[r][c])
+                heapq.heappush(heap, (ndiff, nr, nc))
+
+        
+
