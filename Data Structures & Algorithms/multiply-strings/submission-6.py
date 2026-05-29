0ac554ca6f1af26a9c8fd19c0f3017a35f693030
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-05-29 16:00:19 -0700

    Add: multiply-strings - submission-6

---
+++ Data Structures & Algorithms/multiply-strings/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        result = [0] * (len(num1) + len(num2))
+
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) - 1, -1, -1):
+                idx = i + j + 1
+                temp = result[idx] + int(num1[i]) * int(num2[j])
+                # 当前位保留个位
+                result[idx] = temp % 10
+                # 进位加到前一位
+                result[idx - 1] += temp // 10
+
+        return "".join([str(c) for c in result]) if result[0] != 0 else "".join([str(c) for c in result[1:]])
