a345631e2499cff90f5b1523e616963e6354d518
zebang.eth <112261071+zebangeth@users.noreply.github.com>
2026-06-01 11:17:20 -0700

    Add: decode-string - submission-5

---
+++ Data Structures & Algorithms/decode-string/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        str_stack, num_stack = [], []
+        cur_str, cur_num = "", 0
+        for c in s:
+            if c.isdigit():
+                cur_num = 10 * cur_num + int(c)
+            elif c.isalpha():
+                cur_str += c
+            elif c == '[':
+                str_stack.append(cur_str)
+                num_stack.append(cur_num)
+                cur_str, cur_num = "", 0
+            elif c == ']':
+                last_str = str_stack.pop()
+                last_num = num_stack.pop()
+                cur_str = last_str + last_num * cur_str
+            else:
+                raise ValueError("invalid char in the input s")
+
+        return cur_str
+
+# str_stack = []
+# num_stack = [2]
+# cur_str = abbb
+# cur_num = 
